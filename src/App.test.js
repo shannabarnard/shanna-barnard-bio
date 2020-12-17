2c0c0bea08f2dc1d2a2c4bde651839d98bf61347
shannabarnard <shannabarnard@gmail.com>
2020-12-17 21:19:01 +0000

    Add remaining component tests (issue discovered in previous commit)

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import User from './components/User'
+import Resume from './components/Resume'
+import Highlights from './components/Highlights'
+import Description from './components/Description'
 
 test('renders User (username) component', () => {
   const user = { name: 'username', avatar: 'avatar' }
@@ -8,4 +11,22 @@ test('renders User (username) component', () => {
   const { getByText } = render(<User user={user} />)
   const username = getByText(/username/i)
   expect(username).toBeInTheDocument()
+})
+
+test('renders Resume component', () => {
+  const { getByText } = render(<Resume resume='text resume' />)
+  const text = getByText(/text resume/i)
+  expect(text).toBeInTheDocument()
+})
+
+test('renders Highlights component', () => {
+  const { getByText } = render(<Highlights highlights='text highlights' />)
+  const text = getByText(/text highlights/i)
+  expect(text).toBeInTheDocument()
+})
+
+test('renders Description component', () => {
+  const { getByText } = render(<Description description='text description' />)
+  const text = getByText(/text description/i)
+  expect(text).toBeInTheDocument()
 })
