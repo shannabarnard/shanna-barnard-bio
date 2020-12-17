1171333190d549d012e3f19fefd50fc86fe94532
shannabarnard <shannabarnard@gmail.com>
2020-12-17 20:27:34 +0000

    Setup highlights component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import { AppContext } from './contexts/AppContext'
 import User from './components/User'
 import Resume from './components/Resume'
+import Highlights from './components/Highlights'
+
 // import './App.css';
 
 function App() {
@@ -11,12 +13,13 @@ function App() {
     return null
   }
 
-  const { user, resume } = data.payload
+  const { user, resume, highlights } = data.payload
   return (
     <div className="App">
       <div className="main-content">
         <User user={user} />
         <Resume user={resume} />
+        <Highlights user={highlights} />
       </div>
     </div>
   )
---
+++ src/components/Highlights.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Highlights ({ Highlights }) {
+  return (
+    <div>
+      <h5>
+        Career highlights
+      </h5>
+      <p>{Highlights}</p>
+    </div>
+  )
+}
+
+export default Highlights
