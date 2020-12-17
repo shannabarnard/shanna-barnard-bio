71e9718b146e8109a7c84c4b76f9653ac96944fc
shannabarnard <shannabarnard@gmail.com>
2020-12-17 23:31:39 +0000

    User component styling and text update

--- src/components/User.js
+++ src/components/User.js
@@ -3,10 +3,10 @@ import React from 'react'
 function User({ user }) {
   return (
     <div className="user">
-      <div className="">
+      <div className="user__image">
         <img src={user.avatar} alt="Shanna Barnard avatar" />
       </div>
-      <div className="">
+      <div className="user__details">
         <h1>{user.name} </h1>
         <h3>{user.position}</h3>
       </div>
