1d386b752fd530149a9c658d04d6608d5e57fc9f
shannabarnard <shannabarnard@gmail.com>
2020-12-17 23:23:48 +0000

    Fix grammar of profile info

--- api/src/routes/profile.js
+++ api/src/routes/profile.js
@@ -13,9 +13,9 @@ profile.get('/', (request, response) => {
         position: 'Front End Developer',
       },
       resume:
-        'I\'m going to be super lazy as I am now running out of time(11pm). CV with Dan, although original idea was some sort of career timeline.',
-      highlights: `I like to mentor, for a little while I taught at a 10-day coding Bootcamp (The Institute of Code) in Bali, Indonesia. HTML, CSS, Javascript and the other tools / libraries required to build, deploy and host responsive websites.`,
-      description: `Open-minded, pragmatic developer who is comfortable with change. Confident to voice up when I am unable to deliver. I want my colleagues to like me and the best way I know how to do that is to do a good job. As developers, we have the privilege to learn and teach daily. I like working with others and always aim to make their experience working with me a positive one.`,
+        'I\'m going to be super lazy as I am now running out of time. CV with Dan, although original idea was some sort of career timeline.',
+      highlights: `I like to mentor, for a little while I taught several 10-day coding Bootcamp's (The Institute of Code) in Bali, Indonesia. HTML, CSS, Javascript and the tools/libraries/frameworks required to build, deploy and host responsive websites. Most of my students were women, and I found in an industry dominated by dudes; fair play, I managed to explain concepts from a different perspective. I felt real engagement from my students which sparked a passion in me to get more girls comfortable with tech hopefully one day improving the current imbalance. I'm part of, follow and support a lot of these women in tech groups and am continuously working on my contribution to this.`,
+      description: `Open-minded, pragmatic developer who is comfortable with change. Confident to voice up when I am unable to deliver. I want my colleagues to like me, and the best way I know how to do that is to do a good job. As developers, we have the privilege to learn and teach daily. I like working with others and always aim to make their experience working with me a positive one.`,
     },
   })
 })
